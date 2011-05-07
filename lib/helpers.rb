3683733f855db95dd1657ba1029e2cd23f6e2e73
Paul Saikko <paul.saikko@helsinki.fi>
2011-05-07 15:26:58 +0300

    Cleaning up code

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -3,3 +3,7 @@ include Rack::Utils
 def h text 
 	escape_html(text)
 end
+
+def is_admin? user
+  user && user.admin
+end
--- tsoha.rb
+++ tsoha.rb
@@ -30,38 +30,33 @@ class Tsoha < Sinatra::Base
   end
 
   get '/' do
-    if flash[:query]
-      @query = flash[:query]
-      @items = Item.all(:name.like => "%" + flash[:query] + "%", :expires_at.gt => Time.now)
-    else
-      @query = ""
-      @items = Item.all(:expires_at.gt => Time.now)
-    end
-    
-    case flash[:sort]
-    when "Item name"
-      @items = @items.all(:order => [:name.asc])
-    when "Seller"
-      @items = @items.all.sort_by{ |item| item.user.name }
-    when "Time left"
-      @items = @items.all(:order => [:expires_at.desc])
-    when "Current price"
-      @items = @items.all.sort_by{ |item| item.current_price }
-    end
-    
-    if flash[:reverse] == "reverse"
-      @items.reverse!
-    end
-    
+    @query = ""
+    @items = Item.all(:expires_at.gt => Time.now)
     @users = User.all
     haml :index
   end
   
   post '/' do
-    flash[:query] = params[:query]
-    flash[:sort] = params[:sort]
-    flash[:reverse] = params[:reverse]
-    redirect '/'
+    @query = params[:query]
+    @items = Item.all(:name.like => "%" + @query + "%", :expires_at.gt => Time.now)
+    
+    case params[:sort]
+    when "Item name"
+      @items = @items.all(:order => [:name.asc])
+    when "Seller"
+      @items = @items.all.sort_by{ |item| item.user.name }
+    when "Time left"
+      @items = @items.all(:order => [:expires_at.asc])
+    when "Current price"
+      @items = @items.all.sort_by{ |item| item.current_price }
+    end
+    
+    if params[:reverse] == "reverse"
+      @items.reverse!
+    end
+    
+    @users = User.all
+    haml :index
   end
 
   get '/login' do
